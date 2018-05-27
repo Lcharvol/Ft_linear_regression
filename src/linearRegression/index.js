f458512963f379b3665222a28e93b4428c97c4c8
Lcharvol <charvolin0@gmail.com>
2018-05-27 17:17:19 +0200

    WIP

--- src/linearRegression/index.js
+++ src/linearRegression/index.js
@@ -7,26 +7,25 @@ import {
     LEARNING_RATE,
 } from './constants';
 
-const getEstimatePrice = (mileAge, theta0, theta1, maxKm) => theta0 + (theta1 * mileAge);
+const getEstimatePrice = (mileAge, theta0, theta1) => theta0 + (theta1 * mileAge);
 
-const somTheta0 = (m, data, theta0, theta1, maxKm) => {
+const somTheta0 = (m, data, theta0, theta1) => {
 
     let res = 0;
 
     times(i => {
-        res += (getEstimatePrice(data[i].km, theta0, theta1, maxKm) - data[i].price);
-    },m);
-    return res;
+        res += (getEstimatePrice(data[i].km, theta0, theta1) - data[i].price);
+    },m - 1);
+    return res/m;
 };
 
-const somTheta1 = (m, data, theta0, theta1, maxKm) => {
+const somTheta1 = (m, data, theta0, theta1) => {
 
     let res = 0;
     times(i => {
-        console.log('res: ', (getEstimatePrice(data[i].km, theta0, theta1, maxKm) - data[i].price) * data[i].km);
-        res += (getEstimatePrice(data[i].km, theta0, theta1, maxKm) - data[i].price) * data[i].km;
-    },m);
-    return res;
+        res += ((getEstimatePrice(data[i].km, theta0, theta1) - data[i].price) * data[i].km);
+    },m - 1);
+    return res/m;
 };
 
 export const startLinearRegression = dataStruct => {
@@ -39,11 +38,11 @@ export const startLinearRegression = dataStruct => {
     const learningRate = LEARNING_RATE;
     const m = length(data);
     times(i => {
-        tmpTheta0 = learningRate * (1/m) * somTheta0(m, data, theta0, theta1, maxKm);
-        tmpTheta1 = learningRate * (1/m) * somTheta1(m, data, theta0, theta1, maxKm);
-        theta0 = tmpTheta0;
-        theta1 = tmpTheta1;
+        tmpTheta0 = learningRate * somTheta0(m, data, theta0, theta1, maxKm);
+        tmpTheta1 = learningRate * somTheta1(m, data, theta0, theta1, maxKm);
+        theta0 = theta0 - tmpTheta0;
+        theta1 = theta1 - tmpTheta1;
     },ITERATIONS_NUMBER);
     setTheta0(theta0 * maxPrice);
-    setTheta1(theta1 * maxKm / maxPrice);
+    setTheta1(theta1 * (maxKm / maxPrice));
 };
